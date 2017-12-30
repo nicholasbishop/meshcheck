6ebd99af7f4c785007bfdc3af19580b66b2753cd
Nicholas Bishop <nicholasbishop@gmail.com>
2017-12-30 15:07:03 -0500

    Improve shader loading

--- meshcheck/app.py
+++ meshcheck/app.py
@@ -1,11 +1,66 @@
 import argparse
 import json
+import os
 
 import attr
 import ModernGL
 import numpy
 
 from meshcheck import window
+
+
+class ShaderError(RuntimeError):
+    pass
+
+
+@attr.s(frozen=True, slots=True)
+class ShaderCode:
+    vert = attr.ib(default=None)
+    geom = attr.ib(default=None)
+    frag = attr.ib(default=None)
+
+    @classmethod
+    def load(cls, base_name):
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        shader_dir = os.path.join(script_dir, 'shaders')
+        shader_dir = os.path.relpath(shader_dir)
+        base_path = os.path.join(shader_dir, base_name)
+        codes = cls(**{kind: cls._load_one(base_path, kind)
+                          for kind in cls.__slots__})
+        if not codes.vert:
+            raise ShaderError('vert shader is required')
+        if not codes.vert:
+            raise ShaderError('frag shader is required')
+        return codes
+
+    @staticmethod
+    def _load_one(base_path, kind):
+        try:
+            path = '{}.{}.glsl'.format(base_path, kind)
+            with open(path) as rfile:
+                print('loading {}'.format(path))
+                return rfile.read()
+        except IOError:
+            return None
+
+    def create_shader(self, ctx, kind):
+        funcs = {
+            'vert': 'vertex_shader',
+            'frag': 'fragment_shader',
+            'geom': 'geometry_shader',
+        }
+        func = getattr(ctx, funcs[kind])
+        code = getattr(self, kind)
+        return func(code)
+
+    def has_shader(self, kind):
+        return getattr(self, kind) is not None
+
+    def create_program(self, ctx):
+        shaders = [self.create_shader(ctx, kind)
+                   for kind in self.__slots__
+                   if self.has_shader(kind)]
+        return ctx.program(shaders)
 
 
 class MeshCheckWindow(window.Window):
@@ -41,22 +96,8 @@ class MeshCheckWindow(window.Window):
         return vao
 
     def initialize(self, ctx):
-        prog = ctx.program([
-            ctx.vertex_shader('''
-                #version 330
-                in vec3 vert;
-                void main() {
-                        gl_Position = vec4(vert, 1.0);
-                }
-        '''),
-            ctx.fragment_shader('''
-                #version 330
-                out vec4 color;
-                void main() {
-                        color = vec4(0.3, 0.5, 1.0, 1.0);
-                }
-        '''),
-        ])
+        shader_code = ShaderCode.load('basic')
+        prog = shader_code.create_program(ctx)
 
         self._vert_vbo = self._make_vert_vbo(ctx)
         self._tria_vao = self._make_tria_vao(ctx, prog)
---
+++ meshcheck/shaders/basic.frag.glsl
@@ -0,0 +1,7 @@
+#version 330
+
+out vec4 color;
+
+void main() {
+  color = vec4(0.3, 0.5, 1.0, 1.0);
+}
